8dc7720c1c52f8b2c6717bf1247da9b3fb3f6d92
mrcgkc <quizclubmrc@gmail.com>
2026-07-04 10:29:14 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Keep project configuration separated from your main visual display logic
 const SUPABASE_CONFIG = {
-    URL: "https://your-actual-supabase-url.supabase.co",
-    ANON_KEY: "your-actual-long-public-anon-key-string"
+    URL: "https://qcocrpedxzwntzsexemp.supabase.co",
+    ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFjb2NycGVkeHp3bnR6c2V4ZW1wIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY4MTIxNjksImV4cCI6MjA3MjM4ODE2OX0.F7oTHA8xzGyI6gvmmNN-qxk41Ut9DujFM5qK6ggNypw"
 };
